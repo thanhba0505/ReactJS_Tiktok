257d58a585ac1000c02ed97ba6b583f24710c6dd
Literary_Oasis <lethanhba0505@gmail.com>
2024-01-03 21:53:55 +0700

    default layout

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,6 +1,12 @@
+import classNames from 'classnames/bind';
+import styles from './Sidebar.module.scss'
+
+const cx = classNames.bind(styles)
 
 function Silebar() {
-  return <h2>Silebar</h2>;
+  return <aside className={cx('wrapper')}>
+    <h2 className={cx('')}>Sidebar</h2>
+  </aside>;
 }
 
 export default Silebar;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -4,7 +4,11 @@ import styles from './Header.module.scss';
 const cx = className.bind(styles);
 
 function Header() {
-  return <header className={cx('wrapper')}></header>;
+  return (
+    <header className={cx('wrapper')}>
+      <div className={cx('inner')}></div>
+    </header>
+  );
 }
 
 export default Header;
